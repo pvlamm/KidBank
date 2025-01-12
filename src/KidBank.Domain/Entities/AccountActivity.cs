5aa0f6c1d38b00ff361801412ccca2c1595f3cbc
Patrick Lamm <lammster@gmail.com>
2025-01-12 12:04:42 -0500

    Added 4 entities

---
+++ src/KidBank.Domain/Entities/AccountActivity.cs
@@ -0,0 +1,25 @@
+﻿namespace KidBank.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class AccountActivity
+{
+    public Guid Id { get; set; }
+    public Guid SubAccountId { get; set; }
+    public Guid MainAccountId { get; set; }
+    public Guid? MemoPostId { get; set; }
+    public string MainAccountNumber { get; set; }
+    public string SubAccountNumber { get; set; }
+    public string Description { get; set; }
+    public DateTime TransactionDate { get; set; }
+    public DateTime? TransactionCompleteDate { get; set; }
+    public DateTime? TransactionCancelDate { get; set; }
+    public long Amount { get; set; }
+
+
+
+
+}
---
+++ src/KidBank.Domain/Entities/Customer.cs
@@ -0,0 +1,19 @@
+﻿namespace KidBank.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Customer
+{
+    public Customer()
+    {
+    }
+    public Guid Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    public string EmailAddress { get; set; }
+    public string Phone { get; set; }
+}

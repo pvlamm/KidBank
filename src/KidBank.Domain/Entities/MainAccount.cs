fe4dc661e72156bae7fb751804e67102b02aeeec
Patrick Lamm <lammster@gmail.com>
2025-01-12 12:07:41 -0500

    Added 4 entities (#1)
    * Added 4 entities
    
    * Removed Excess Spaces!!

---
+++ src/KidBank.Domain/Entities/MainAccount.cs
@@ -0,0 +1,22 @@
+﻿namespace KidBank.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class MainAccount
+{
+    public MainAccount()
+    {
+        this.SubAccounts = new List<SubAccount>();
+    }
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string AccountNumber { get; set; }
+    public long Balance { get; set; }
+   
+    public DateOnly OpenDate { get; set; }
+    public DateOnly? CloseDate { get; set; }
+    public List<SubAccount> SubAccounts { get; set; }
+}
---
+++ src/KidBank.Domain/Entities/SubAccount.cs
@@ -0,0 +1,20 @@
+﻿namespace KidBank.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class SubAccount
+{
+    public Guid Id { get; set; }
+    public Guid ParentId { get; set; }
+    public string Name { get; set; }
+    public string MainAccountNumber { get; set; }
+    public string AccountNumber { get; set; }
+
+    public long Balance { get; set; }
+
+    public DateOnly OpenDate { get; set; }
+    public DateOnly? CloseDate { get; set; }
+}
